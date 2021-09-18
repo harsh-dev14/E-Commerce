a021373ed75009ecb6320367c62d0eb13e251fcf
harsh-dev14 <devharsh1403@gmail.com>
2021-09-18 20:59:59 +0530

    updated backend
    Signed-off-by: harsh-dev14 <devharsh1403@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const app = express()
 const products= require('./data/products')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
 
 app.get('/', (req, res) => {
     res.send('API is running');
@@ -15,6 +19,8 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 });
 
-app.listen(5000, () => {
-    console.log('App listening on port 5000!');
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
